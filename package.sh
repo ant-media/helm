78b97ca48688ced220e91e888e2f7d13aa25c565
Murat Ugur Eminoglu <murat@murat.ws>
2023-06-07 10:41:54 +0300

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 rm $(pwd)/antmedia-"$(cat VERSION)".tgz
 sed -i -e "s/^version.*/version: $(cat VERSION)/" -e "s/^appVersion.*/appVersion: $(cat VERSION)/" $(pwd)/Chart.yaml
 sed -i "s/tag:.*/tag: $(cat VERSION)/" "$(pwd)"/values.yaml
