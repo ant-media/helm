d4d5e8d470cd3c5fa5776eab891b942c21b82670
Murat Ugur Eminoglu <murat@murat.ws>
2023-06-07 10:37:17 +0300

    Fix sonarcloud (SC2046)

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm $(pwd)/antmedia-$(cat VERSION).tgz
+rm $(pwd)/antmedia-"$(cat VERSION)".tgz
 sed -i -e "s/^version.*/version: $(cat VERSION)/" -e "s/^appVersion.*/appVersion: $(cat VERSION)/" $(pwd)/Chart.yaml
-sed -i "s/tag:.*/tag: $(cat VERSION)/" $(pwd)/values.yaml
+sed -i "s/tag:.*/tag: $(cat VERSION)/" "$(pwd)"/values.yaml
 helm dependency update
-helm package $(pwd)/.
+helm package "$(pwd)"/.
 helm repo index --url https://ant-media.github.io/helm/ --merge index.yaml .
